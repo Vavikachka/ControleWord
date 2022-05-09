eaa51ad95baca6d6c21b99678e9415c73f0900e2
Vika <vavikachka48gmail.com>
2022-05-09 13:13:12 +0300

    end

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿int count=3;
-string[] Array1= {"3","Vika","))","90","File","78","City"};
+string[] Array1= {"383","Vika","Bom","90","File","78","City"};
 string [] Array2= new string [count];
+Console.Write("{");
 ChangesArray(Array2,Array1);
+Console.Write("}"); 
+
 
 void ChangesArray (string [] arr2, string [] arr1){
     for(int i=0;i<arr2.Length;i++){
