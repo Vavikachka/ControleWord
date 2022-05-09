ab93818678da39c96ec6ffe2a22cfba8643c1810
Vika <vavikachka48gmail.com>
2022-05-09 13:03:26 +0300

    add method

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿int count=3;
 string[] Array1= {"3","Vika","))","90","File","78","City"};
 string [] Array2= new string [count];
+ChangesArray(Array2,Array1);
+
+void ChangesArray (string [] arr2, string [] arr1){
+    for(int i=0;i<arr2.Length;i++){
+        arr2[i]=arr1[i];
+        Console.Write($"{arr2[i]} ");
+    }
+}
